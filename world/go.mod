fd34e72212f3680690351ddf378213a665d505a0
Kai Hendry <hendry@iki.fi>
2022-01-08 17:42:15 +0800

    Make go offlineable

--- world/go.mod
+++ world/go.mod
@@ -2,4 +2,12 @@ module github.com/kaihendry/sls-two-languages
 
 go 1.17
 
-require github.com/aws/aws-lambda-go v1.28.0 // indirect
+require (
+	github.com/apex/gateway/v2 v2.0.0
+	github.com/apex/log v1.9.0
+)
+
+require (
+	github.com/aws/aws-lambda-go v1.28.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+)
--- world/main.go
+++ world/main.go
@@ -1,45 +1,48 @@
 package main
 
 import (
-	"bytes"
-	"context"
-	"encoding/json"
+	"embed"
+	"fmt"
+	"html/template"
+	"net/http"
+	"os"
+	"time"
 
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/apex/gateway/v2"
+	"github.com/apex/log"
+	jsonhandler "github.com/apex/log/handlers/json"
+	"github.com/apex/log/handlers/text"
 )
 
-// Response is of type APIGatewayProxyResponse since we're leveraging the
-// AWS Lambda Proxy Request functionality (default behavior)
-//
-// https://serverless.com/framework/docs/providers/aws/events/apigateway/#lambda-proxy-integration
-type Response events.APIGatewayProxyResponse
-
-// Handler is our lambda handler invoked by the `lambda.Start` function call
-func Handler(ctx context.Context) (Response, error) {
-	var buf bytes.Buffer
-
-	body, err := json.Marshal(map[string]interface{}{
-		"message": "haha! so your other function also executed successfully!",
-	})
-	if err != nil {
-		return Response{StatusCode: 404}, err
-	}
-	json.HTMLEscape(&buf, body)
-
-	resp := Response{
-		StatusCode:      200,
-		IsBase64Encoded: false,
-		Body:            buf.String(),
-		Headers: map[string]string{
-			"Content-Type":           "application/json",
-			"X-MyCompany-Func-Reply": "world-handler",
-		},
-	}
-
-	return resp, nil
-}
+//go:embed templates
+var tmpl embed.FS
 
 func main() {
-	lambda.Start(Handler)
+	t, err := template.ParseFS(tmpl, "templates/*.html")
+	if err != nil {
+		log.WithError(err).Fatal("Failed to parse templates")
+	}
+
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		rw.Header().Set("Content-Type", "text/html")
+		err = t.ExecuteTemplate(rw, "index.html", struct {
+			Now time.Time
+		}{
+			time.Now(),
+		})
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			log.WithError(err).Fatal("Failed to execute templates")
+		}
+	})
+
+	log.SetHandler(jsonhandler.Default)
+
+	if _, ok := os.LookupEnv("AWS_LAMBDA_FUNCTION_NAME"); ok {
+		err = gateway.ListenAndServe("", nil)
+	} else {
+		log.SetHandler(text.Default)
+		err = http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), nil)
+	}
+	log.WithError(err).Fatal("error listening")
 }
